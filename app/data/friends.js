92dd9a4b60a085ad8c1c4dfdc3585ae70d8a4225
abhishek0mukherjee <abhishek0mukherjee@gmail.com>
2017-04-22 20:09:00 -0400

    friend

---
+++ app/data/friends.js
@@ -0,0 +1,181 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Rachel Green',
+		photo: 'https://s-media-cache-ak0.pinimg.com/originals/83/dc/5d/83dc5d2a78cfe85ce5f663d28b8de789.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Monica Geller',
+		photo: 'http://vignette1.wikia.nocookie.net/friends/images/c/c2/Monica-Gellar.jpg/revision/latest?cb=20130802071243',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Joey Tribbiani',
+		photo: 'https://typeset-beta.imgix.net/rehost/2016/9/13/93d8d112-7334-4878-b556-e3d67fd91412.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Chandler Bing',
+		photo: 'http://nypdecider.files.wordpress.com/2014/09/friends-chandler.png',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Ross Geller',
+		photo: 'https://i.ytimg.com/vi/1HkqeORgn_U/maxresdefault.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Phoebe Buffay',
+		photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-11/25/10/campaign_images/webdr10/phoebe-from-friends-is-literally-the-fucking-worst-2-8032-1448464781-1_dblbig.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Janice Hosenstein',
+		photo: 'https://s-media-cache-ak0.pinimg.com/736x/da/af/d3/daafd3a114bc490833dc89c2a49eb3d6.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Barney Stinson',
+		photo: 'http://images.buddytv.com/userquizimages/q-1/e4a7c763-1c03-4487-a2d8-432ea4320481magicbarney.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Robin Scherbatsky',
+		photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-01/10/9/campaign_images/buzzfeed-prod-fastlane-03/18-times-robin-from-how-i-met-your-mother-spoke-t-2-24402-1484058598-0_dblbig.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ted Mosby',
+		photo: 'https://typeset-beta.imgix.net/rehost/2016/9/13/c6938568-59a1-445c-9c6c-cf6de00f0c06.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lily Aldrin',
+		photo: 'http://latimesblogs.latimes.com/.a/6a00d8341c630a53ef0162fde70558970d-pi',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
